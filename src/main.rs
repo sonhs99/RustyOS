fe9581f518e8e822853d19408c65637162dd2d1e
sonhs99 <sonhs99@gmail.com>
2023-05-08 21:55:58 +0900

    start

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, World{}", '!');
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial_assertion... ");
+    assert_eq!(1, 1);
+    println!("[OK]");
+}
